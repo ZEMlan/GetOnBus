e02ceb607bdc67ccaeaa5c3e1853f5ba18178d87
Eugeniya <zem.jane@mail.ru>
2020-02-18 09:41:28 +0500

    v0.1. final heading

--- app/src/main/java/ru/zemlyanaya/getonbus/MainActivity.kt
+++ app/src/main/java/ru/zemlyanaya/getonbus/MainActivity.kt
@@ -1,37 +1,37 @@
 package ru.zemlyanaya.getonbus
 
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.Shader.TileMode
 import android.os.Bundle
+import android.text.TextPaint
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import kotlinx.android.synthetic.main.head_bar.*
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
+        initUI()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_map -> {
-                //TODO("Add map fragment")
-                true
-            }
-            R.id.action_profile ->{
-                //TODO("Add profile fragment")
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+    private fun initUI(){
+        val textShader: Shader = LinearGradient(
+            0f,
+            0f,
+            label.text.length.toFloat(),
+            label.textSize,
+            intArrayOf(Color.rgb(60, 111, 250),
+                Color.rgb(71, 120, 254),
+                Color.rgb(105, 155, 254),
+                Color.rgb(142, 173, 241)
+                ),
+            floatArrayOf(0.1f, 0.7f, 0.9f, 1f),
+            TileMode.CLAMP
+        )
+        label.paint.shader = textShader
     }
 }
